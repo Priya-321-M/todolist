d4e6f8ff7b7a87054ceae70eea4b2049f6cd1499
Priya-321-M <113544517+Priya-321-M@users.noreply.github.com>
2022-11-19 15:30:19 +0530

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,48 +1,43 @@
-const todoList=require('../todo.js');
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  }
+  const overdue = () => {  
+  return all.filter((item) => item.dueDate < new Date().toLocaleDateString("en-CA"));
+       
+  }
+  const dueToday = () => { 
+    return all.filter((item) => item.dueDate === new Date().toLocaleDateString("en-CA"));
+         
+  }
+  const dueLater = () => { 
+    return all.filter((item) => item.dueDate > new Date().toLocaleDateString("en-CA"));
+  }
 
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+  const toDisplayableList = (list) => {  
+    return (list.map(item=>` ${item.completed ? '[x]' : '[ ]'} ${item.title} ${item.dueDate === today? "": item.dueDate }`)).join("\n");
+                                  
+   
+}
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
 
-describe("TodoList test suite", () => {
-  beforeAll(() => {
-    const formattedDate = (d) => {
-      return d.toISOString().split("T")[0];
-    };
-    var dateToday = new Date();
-    const today = formattedDate(dateToday);
-    const yesterday = formattedDate(
-      new Date(new Date().setDate(dateToday.getDate() - 1))
-    );
-    const tomorrow = formattedDate(
-      new Date(new Date().setDate(dateToday.getDate() + 1))
-    );
-    add(
-      { title: "Pay rent", dueDate: yesterday, completed: true },
-      { title: "Pay rent", dueDate: today, completed: true },
-      { title: "Service Vehicle", dueDate: today, completed: false },
-      { title: "File taxes", dueDate: tomorrow, completed: false },
-      { title: "Pay electric bill", dueDate: tomorrow, completed: false }
-    );
-  });
-  test("Should add new items", () => {
-    const ItemsCount = all.length;
-    add({
-      title: "Pay rent",
-      dueDate: new Date().toLocaleDateString("en-CA"),
-      completed: true,
-    });
-    expect(all.length).toBe(ItemsCount + 1);
-  });
-  test("Mark mark todo as completed", () => {
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-  test("Mark todo overdue items", () => {
-    expect(overdue().length).toEqual(1);
-  });
-  test("Mark todo dueToday items", () => {
-    expect(dueToday().length).toEqual(1);
-  });
-  test("Mark todo dueLater items", () => {
-    expect(dueLater().length).toEqual(0);
-  });
-});
+}
+const todos = todoList();
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+module.exports = todoList;
